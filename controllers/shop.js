1b89eb3c4fb2fa940bfcf09e12c0fb5b0277c405
reuben-k-o <rubenkhaemba@gmail.com>
2022-02-15 11:18:26 +0300

    display cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -20,7 +20,10 @@ exports.postCart = (req, res, next) => {
     .then((product) => {
       return req.user.addToCart(product);
     })
-    .then((result) => console.log(result));
+    .then((result) => {
+      console.log(result);
+      res.redirect("/cart");
+    });
 
   // let fetchedCart;
   // let newQuantity = 1;
@@ -83,14 +86,11 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then((cart) => {
-      // console.log(cart);
-      return cart.getProducts().then((products) => {
-        res.render("shop/cart", {
-          path: "/cart",
-          pageTitle: "Your Cart",
-          products,
-        });
+    .then((products) => {
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products,
       });
     })
     .catch((err) => console.log(err));
