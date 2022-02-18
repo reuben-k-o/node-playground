54709e93f1577ecab8bae87a3ea7a18c62356dcc
reuben-k-o <rubenkhaemba@gmail.com>
2022-02-18 17:35:23 +0300

    fixed login options

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,7 +7,7 @@ exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticasted: false,
+    isAuthenticated: false,
   });
 };
 
