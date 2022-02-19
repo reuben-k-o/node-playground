6874add34bcd490f4b19bacd50fb4cee6285ff45
reuben-k-o <rubenkhaemba@gmail.com>
2022-02-19 22:54:10 +0300

    authorization

--- controllers/admin.js
+++ controllers/admin.js
@@ -71,21 +71,23 @@ exports.postEditProduct = (req, res, next) => {
 
   Product.findById(prodId)
     .then((product) => {
+      if (userId.toString() !== req.user._id.toString()) {
+        return res.redirect("/");
+      }
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.imageUrl = updatedImageUrl;
       product.description = updatedDescription;
-      return product.save();
-    })
-    .then((result) => {
-      console.log("Updated product!!!!");
-      res.redirect("/admin/products");
+      return product.save().then((result) => {
+        console.log("Updated product!!!!");
+        res.redirect("/admin/products");
+      });
     })
     .catch((err) => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  Product.find({ userId: req.user._id })
     // .select("title price -_id")
     // .populate("userId", 'name')
     .then((products) => {
@@ -101,7 +103,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByIdAndRemove(prodId)
+  // Product.findByIdAndRemove(prodId)
+  Product.deleteOne({ _id: prodId, userId: req.user._id })
     .then(() => {
       console.log("Destroyed Product!!");
       res.redirect("/admin/products");
