f6216f2bf5ad9a03100ede3d65e1ea30d6ffea03
reuben-k-o <rubenkhaemba@gmail.com>
2022-02-19 19:52:42 +0300

    reseting passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require("crypto");
 const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
 const sendgridTransport = require("nodemailer-sendgrid-transport");
@@ -116,21 +117,102 @@ exports.postLogout = (req, res, next) => {
     console.log(err);
     res.redirect("/");
   });
+};
 
-  exports.getReset = (req, res, next) => {
-    let message = req.flash("error");
-    if (message.length > 0) {
-      message = message[0];
-    } else {
-      message = null;
+exports.getReset = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset Password",
+    isAuthenticated: false,
+    errorMessage: message,
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect("/reset");
     }
-    res.render("auth/reset", {
-      path: "/reset",
-      pageTitle: "Reset Password",
-      isAuthenticated: false,
-      errorMessage: message,
-    });
-  };
+    const token = buffer.toString("hex");
 
-  exports.postReset = (req, res, next) => {};
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash("error", "No account with such email found!");
+          return res.redirect("/reset");
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then((result) => {
+        res.redirect("/");
+        return transporter.sendMail({
+          to: req.body.email,
+          from: "rubenkhaemba@gmail.com",
+          subject: "Reset Password",
+          html: `
+          <p> You requested a password reset for your account.</p>
+          <p> click this <a href="http://localhost:3000/reset/${token}"> link </a> to set a new password </p>
+          `,
+        });
+      })
+      .catch((err) => console.log(err));
+  });
+};
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then((user) => {
+      let message = req.flash("error");
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render("auth/new-password", {
+        path: "/new-password",
+        pageTitle: "New Password",
+        isAuthenticated: false,
+        errorMessage: message,
+        passwordToken: token,
+        userId: user._id.toString(),
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
+    })
+    .catch((err) => console.log(err));
 };
--- routes/auth.js
+++ routes/auth.js
@@ -5,10 +5,12 @@ const authController = require("../controllers/auth");
 router.get("/login", authController.getLogin);
 router.get("/signup", authController.getSignup);
 router.get("/reset", authController.getReset);
+router.get("/reset/:token", authController.getNewPassword);
 
 router.post("/signup", authController.postSignup);
 router.post("/login", authController.postLogin);
 router.post("/reset", authController.postReset);
 router.post("/logout", authController.postLogout);
+router.post("/new-password", authController.postNewPassword);
 
 module.exports = router;
