99acb3dd0b85ae878e1895b115640c0deace3363
reuben-k-o <rubenkhaemba@gmail.com>
2022-02-20 08:44:53 +0300

    validating email and password

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { check } = require("express-validator");
 const authController = require("../controllers/auth");
 
 router.get("/login", authController.getLogin);
@@ -7,7 +8,34 @@ router.get("/signup", authController.getSignup);
 router.get("/reset", authController.getReset);
 router.get("/reset/:token", authController.getNewPassword);
 
-router.post("/signup", authController.postSignup);
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom((value, { req }) => {
+        if (value === "test@test.com") {
+          throw new Error("This email is forbidden");
+        }
+        return true;
+      }),
+    check(
+      "password",
+      "Please enter a password with only numbers and text and atleast 5 characters."
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    check("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords have to match!");
+      }
+      return true;
+    }),
+  ],
+  authController.postSignup
+);
+
 router.post("/login", authController.postLogin);
 router.post("/reset", authController.postReset);
 router.post("/logout", authController.postLogout);
